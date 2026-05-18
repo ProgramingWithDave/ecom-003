c9481bad5462573c44c7f0db8935dbca98465593
dave <tshepomashigo97@gmail.com>
2026-05-19 01:25:21 +0200

    basic app structure done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter, Outfit } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
+const outfit = Outfit({ subsets: ["latin"], weight: ["300", "400", "500"] });
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -25,7 +28,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full text-gray-700", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable, outfit.className)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
